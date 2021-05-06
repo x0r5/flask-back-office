340d604dc655ae1c538f4e826839b8f93accbc4a
x0r5 <obivan05@gmail.com>
2021-05-06 19:21:51 +0200

    more auth

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -15,6 +16,7 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
             else:
                 flash('Incorrect password.', category='error')
         else:
@@ -24,8 +26,10 @@ def login():
     return render_template("login.html")
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "logout"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def signup():
@@ -50,6 +54,7 @@ def signup():
             db.session.add(new_user)
             db.session.commit()
             flash('Account created', category="success")
+            login_user(user, remember=True)
             return redirect(url_for('views.home'))
 
     return render_template("sign_up.html")
