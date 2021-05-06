9b26822e7dc3581f61bb28c15f95a195cf1025e1
x0r5 <obivan05@gmail.com>
2021-05-06 18:01:12 +0200

    html templates

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,31 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return "<p>Login</p>"
+    data = request.form
+    print(data)
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
-    return "<p>Logout</p>"
+    return "logout"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def signup():
-    return "<p>Sign Up</p>"
+    if request.method == 'POST':
+        email = request.form.get('email')
+        name = request.form.get('name')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        terms = request.form.get('terms')
+
+
+        if len(email) < 4:
+            flash('Email must be longer!', category='error')
+        elif password1 != password2:
+            flash('Your passwords do not match!', categor='error')
+        else:
+            flash('Account created', category="success")
+    return render_template("sign_up.html")
